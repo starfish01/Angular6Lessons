b1fdc078a86126656620c27e886232c655d217a9
Patrick Labes <patrick.labes@gmail.com>
2019-01-30 14:14:19 +1000

    lunch time lessons

--- Section 3 - The Basics/my-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Section 3 - The Basics/my-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe','this is a test','https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/IMG_1105.jpg')
+  ];
 
   constructor() { }
 
