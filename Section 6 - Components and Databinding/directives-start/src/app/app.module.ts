84e82eec705dcafdaf217af1d2c8eac092f6d591
Patrick Labes <patrick.labes@gmail.com>
2019-02-02 07:21:26 +1000

    morning lessons - did some work with directives

--- Section 6 - Components and Databinding/directives-start/src/app/app.module.ts
+++ Section 6 - Components and Databinding/directives-start/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { BasicHighlightDirection } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless/unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlightDirection
+    BasicHighlightDirection,
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
---
+++ Section 6 - Components and Databinding/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+
+  @Input() defaultColor: string = 'transparent'
+  @Input() highlightColor: string = 'blue'
+
+  @HostBinding('style.backgroundColor') backgroundColor: String;
+
+
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.backgroundColor = this.defaultColor;
+  }
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit() {
+    this.backgroundColor = this.defaultColor;
+  }
+
+}
