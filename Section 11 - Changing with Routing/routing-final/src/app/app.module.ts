424fe361b9461a5bff1d35354fb17fa9784a9b94
Patrick Labes <patrick.labes@gmail.com>
2019-02-05 07:03:22 +1000

    finished introduction into routing

--- Section 11 - Changing with Routing/routing-final/src/app/app.module.ts
+++ Section 11 - Changing with Routing/routing-final/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { Routes, RouterModule } from "@angular/router"
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -13,13 +12,12 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppRountingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 import { ErrorPageComponent } from './error-page/error-page.component';
-
-
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 @NgModule({
   declarations: [
@@ -37,9 +35,9 @@ import { ErrorPageComponent } from './error-page/error-page.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRountingModule
+    AppRoutingModule
   ],
-  providers: [ServersService,AuthService,AuthGuard,CanDeactivateGuard],
+  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard, ServerResolver],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Section 11 - Changing with Routing/routing-start/src/app/servers/edit-server/can-deactivate-guard.service.ts
+++ Section 11 - Changing with Routing/routing-start/src/app/servers/edit-server/can-deactivate-guard.service.ts
@@ -7,7 +7,10 @@ export interface CanComponentDeactivate {
 }
 
 export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate> {
-    canDeactivate(component: CanComponentDeactivate, currentRoute: ActivatedRouteSnapshot, currentState:RouterStateSnapshot,nextState: RouterStateSnapshot) {
-
+    canDeactivate(component: CanComponentDeactivate, 
+        currentRoute: ActivatedRouteSnapshot, 
+        currentState:RouterStateSnapshot,
+        nextState: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+            return component.canDeactive();
     }
 }
