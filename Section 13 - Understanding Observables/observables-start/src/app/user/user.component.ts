24da880ff42bf8a9d4b3aa8d5e5c12abce570769
Patrick Labes <patrick.labes@gmail.com>
2019-02-06 14:20:30 +1000

    Starting the section on forms

--- Section 13 - Understanding Observables/observables-start/src/app/user/user.component.ts
+++ Section 13 - Understanding Observables/observables-start/src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-user',
@@ -9,7 +10,11 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class UserComponent implements OnInit {
   id: number;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private userService: UsersService) { }
+
+  buttonClicked() {
+    this.userService.userActivated.next(this.id);
+  }
 
   ngOnInit() {
     this.route.params
