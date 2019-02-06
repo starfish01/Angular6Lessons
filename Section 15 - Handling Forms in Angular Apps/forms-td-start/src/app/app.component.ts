68122cb51c4e713ede2031f360c83e22d53a759f
starfish01 <patrick.labes@gmail.com>
2019-02-07 06:35:11 +1000

    learning about forms

--- Section 15 - Handling Forms in Angular Apps/forms-td-start/src/app/app.component.ts
+++ Section 15 - Handling Forms in Angular Apps/forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,17 @@ export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
+
+  // onSubmit(form:NgForm) {
+  //   console.log(form)
+  // }
+
+  @ViewChild('form') signupForm;
+  defaultQuestion = 'pet'
+  genders = ['male','female']
+
+  onSubmit() {
+    console.log(this.signupForm)
+  }
+
 }
