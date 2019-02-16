fdbcb73e01018b0d223a91871a148f63a56b63dd
Patrick Labes <patrick.labes@gmail.com>
2019-02-16 17:06:27 +1000

    working on side project

--- sideprojects/zendesk-helper/src/app/zendesk-body/zendesk-body.component.ts
+++ sideprojects/zendesk-helper/src/app/zendesk-body/zendesk-body.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ZendeskBodyComponent implements OnInit {
 
+  categories = [{category:'Funnel',topic:[{title:'funnel topic 1',description:'Description',displayed:1},{title:'topic 2',description:'Description',displayed:2}],id:1},
+  {category:'Enrol',topic:[{title:'enrol topic 1',description:'Description',displayed:1},{title:'topic 2',description:'Description',displayed:2}],id:2},
+  {category:'Web',topic:[{title:'web topic 1',description:'Description',displayed:1},{title:'topic 2',description:'Description',displayed:2}],id:3},
+  {category:'Eduapp',topic:[{title:'eduapp topic 1',description:'Description',displayed:1},{title:'topic 2',description:'Description',displayed:2}],id:4}]
+  
+  selectedCategory = null;
+
+  selectedTopic = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onCategorySelect(selectedCategory) {
+    console.log(selectedCategory)
+    this.selectedCategory = selectedCategory
+  }
+
+  onTopicSelect(item) {
+    this.selectedTopic = item;
   }
 
 }
