d67311596559bd768adba3b06a3410b859166946
Patrick Labes <patrick.labes@gmail.com>
2019-03-04 14:18:09 +1000

    Looking at NgRx

--- Section 24 - Working with NgRx in our Project/01-ngrx-basics-start/src/app/shared/data-storage.service.ts
+++ Section 24 - Working with NgRx in our Project/01-ngrx-basics-start/src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpRequest } from '@angular/common/http'
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
 import 'rxjs/Rx';
 
 import { RecipeService } from '../recipes/recipe.service';
@@ -14,31 +14,26 @@ export class DataStorageService {
   }
 
   storeRecipes() {
-    const token = this.authService.getToken();
+    // const headers = new HttpHeaders().set('Authorization', 'Bearer afdklasflaldf');
 
-    // return this.httpClient.put('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', this.recipeService.getRecipes(),
-    // {
-    //   observe:'events',
-    //   params: new HttpParams().set('auth',token)
+    // return this.httpClient.put('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
+    //   observe: 'body',
+    //   params: new HttpParams().set('auth', token)
+    //   // headers: headers
     // });
-
-    //This Creates the request were we can watch the progress of the download
-    const req = new HttpRequest('PUT','https://ng-recipe-book-3adbb.firebaseio.com/recipes.json',this.recipeService.getRecipes(), {
-      reportProgress:true,
-      params: new HttpParams().set('auth',token)
-    })
-    //This initiats the request
+    const req = new HttpRequest('PUT', 'https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {reportProgress: true});
     return this.httpClient.request(req);
-
-
   }
 
   getRecipes() {
-    const token = this.authService.getToken();
-
-    this.httpClient.get<Recipe[]>('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json?auth=' + token)
+    // this.httpClient.get<Recipe[]>('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json?auth=' + token)
+    this.httpClient.get<Recipe[]>('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', {
+      observe: 'body',
+      responseType: 'json'
+    })
       .map(
         (recipes) => {
+          console.log(recipes);
           for (let recipe of recipes) {
             if (!recipe['ingredients']) {
               recipe['ingredients'] = [];
